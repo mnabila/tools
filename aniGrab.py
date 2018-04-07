a480d81d1a9b9fffc074520115d283d767bd6901
nabil48 <nblid48@gmail.com>
2018-04-07 23:12:47 +0700

    update script

--- aniGrab.py
+++ aniGrab.py
@@ -6,11 +6,15 @@ list site
 3.drivenime
 4.bakacan
 5.meownime
+6.wibudesu
+7.kusonime
+8.awbatch
 """
 import requests
 from bs4 import BeautifulSoup
 import sys
 import argparse
+import time
 
 class animeLink:
     def __init__(self):
@@ -19,7 +23,10 @@ class animeLink:
             "meguminime":"http://meguminime.com/",
             "drivenime":"https://drivenime.com/",
             "bakacan":"http://bakacan.id/",
-            "meownime":"http://meownime.com/"
+            "meownime":"http://meownime.com/",
+            "wibudesu":"https://wibudesu.com/",
+            "kusonime":"https://kusonime.com/",
+            "awbatch":"http://awbatch.in/"
         }
     def search(self,keyword):
         self.query={
@@ -39,6 +46,17 @@ class animeLink:
             "meownime":{
                 "s":keyword,
                 "submit":"Search"
+            },
+            "wibudesu":{
+                "s":keyword,
+                "post_type":"post"
+            },
+            "kusonime":{
+                "s":keyword,
+                "post_type":"post"
+            },
+            "awbatch":{
+                "s":keyword
             }
         }
     def animesave(self):
@@ -105,10 +123,48 @@ class animeLink:
         except:
             result.append("404 Not Found")
         return result
+    
+    def wibudesu(self):
+        result=[]
+        try:
+            cnx=requests.get(self.url["wibudesu"],params=self.query["wibudesu"])
+            soup=BeautifulSoup(cnx.text, "lxml")
+            div=soup.find(class_="rseries")
+            link=div.find_all("h2")
+            for h2 in link:
+                result.append(h2.a["href"])
+        except:
+            result.append("404 Not Found")
+        return result
+    def kusonime(self):
+        result=[]
+        try:
+            cnx=requests.get(self.url["kusonime"],params=self.query["kusonime"])
+            soup=BeautifulSoup(cnx.text, "lxml")
+            div=soup.find(class_="rseries")
+            link=div.find_all("h2")
+            for h2 in link:
+                result.append(h2.a["href"])
+        except:
+            result.append("404 Not Found") 
+        return result
+
+    def awbatch(self):
+        result=[]
+        try:
+            cnx=requests.get(self.url["awbatch"],params=self.query["awbatch"])
+            soup=BeautifulSoup(cnx.text, "lxml")
+            div=soup.find(class_="konten-tengah")
+            link=div.find_all("h2")
+            for h2 in link:
+                result.append(h2.a["href"])
+        except:
+            result.append("404 Not Found") 
+        return result
 
 def command(argv=None):
     parse=argparse.ArgumentParser(description="tools untuk mencari link download anime")
-    parse.add_argument("-s","--site",help="untuk menentukan website mana yang akan di ambil datanya. jika parameter site tidak diisi maka akan dicari diseluruh website yang ada(1.animesave 2.meguminime 3.drivenime 4.bakacan 5.meownime)")
+    parse.add_argument("-s","--site",help="untuk menentukan website mana yang akan di ambil datanya. jika parameter site tidak diisi maka akan dicari diseluruh website yang ada(1.animesave 2.meguminime 3.drivenime 4.bakacan 5.meownime 6.wibudesu 7.kusonime)")
     parse.add_argument("-k","--keyword",help="kata kunci untuk mencari link")
 
     result=parse.parse_args(argv)
@@ -130,12 +186,28 @@ if __name__ == "__main__":
         result["bakacan"]=anime.bakacan()
     elif site == "meownime":
         result["meownime"]=anime.meownime()
+    elif site == "wibudesu":
+        result["wibudesu"]=anime.wibudesu()
+    elif site == "kusonime":
+        result["kusonime"]=anime.kusonime()
+    elif site == "awbatch":
+        result["awbatch"]=anime.awbatch()
     else:
         result["animesave"]=anime.animesave()
+        time.sleep(1)
         result["meguminime"]=anime.meguminime()
+        time.sleep(1)
         result["drivenime"]=anime.drivenime()
+        time.sleep(1)
         result["bakacan"]=anime.bakacan()
+        time.sleep(1)
         result["meownime"]=anime.meownime()
+        time.sleep(1)
+        result["wibudesu"]=anime.wibudesu()
+        time.sleep(1)
+        result["kusonime"]=anime.kusonime()
+        time.sleep(1)
+        result["awbatch"]=anime.awbatch()
 
     for website in result:
         print("[+] %s"%website)
