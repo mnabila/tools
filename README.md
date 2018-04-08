e6f3450905176910b71cc52b9b3996853e68f122
nabil48 <nblid48@gmail.com>
2018-04-08 15:35:11 +0700

    update aniGrab

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # tools
-### list tools
+## list tools
 + [dir2iso.py](https://github.com/nabil48/tools/blob/master/dir2iso.py)  
-    this tool function for make iso image from directory using genisoimage
+    this tool function for make iso image from directory using genisoimage  
+    **required package:**
+    + argparse (*pip3 install argparse*)
+    + genisoimage (*sudo apt-get install genisoimage*)
 + [fix-dpkg-error.sh](https://github.com/nabil48/tools/blob/master/fix-dpkg-error.sh)  
     fix dpkg error when installing aplication with error message "*dpkg: error: dpkg status database is locked by another process*"
 + [mp43mp3.py](https://github.com/nabil48/tools/blob/master/mp42mp3.py)  
-    convert video with format .mp4 to audio or music with format .mp3 using ffmpeg
+    convert video with format .mp4 to audio or music with format .mp3 using ffmpeg  
+    **required package:**
+    + argparse (*pip3 install argparse*)
+    + ffmpeg (*sudo apt-get install ffmpeg*)
 + [aniGrab.py](https://github.com/nabil48/tools/blob/master/aniGrab.py)  
-    coming soon
+    grabing data from website  
+    **required package:**
+    + requests (*pip3 install requests*)
+    + BeautifulSoup4 (*pip3 install BeautifulSoup4*)
+    + argparse (*pip3 install argparse*)
+    + colorama (*pip3 install colorama*)   
 ##### bahasa ingrisnya ancur coeg wkwkwk
--- aniGrab.py
+++ aniGrab.py
@@ -1,4 +1,6 @@
 """
+author:Nabil
+site:http://github.com/nabil48
 membuat agc untuk download anime
 list site
 1.animesave
@@ -15,7 +17,7 @@ from bs4 import BeautifulSoup
 import sys
 import argparse
 import time
-
+from colorama import Fore,Back,Style
 class animeLink:
     def __init__(self):
         self.url={
@@ -27,6 +29,12 @@ class animeLink:
             "wibudesu":"https://wibudesu.com/",
             "kusonime":"https://kusonime.com/",
             "awbatch":"http://awbatch.in/"
+        }
+        self.header={
+            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Encoding":"gzip, deflate",
+            "Connection":"keep-alive",
+            "User-Agent":"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
         }
     def search(self,keyword):
         self.query={
@@ -62,154 +70,225 @@ class animeLink:
     def animesave(self):
         result=[]
         try:
-            cnx=requests.get(self.url["animesave"],params=self.query["animesave"])
+            cnx=requests.get(self.url["animesave"],params=self.query["animesave"], headers=self.header)
             soup=BeautifulSoup(cnx.text, "lxml")
             div=soup.find(class_="allgreen")
             link=div.find_all("a")
             for a in link:
-                result.append(a["href"])
+                data={}
+                data["link"]=a["href"]
+                data["title"]=a["title"]
+                result.append(data)
         except:
-            result.append("404 Not Found")
+            data={}
+            data["link"]="404 Not Found"
+            data["title"]="404 Not Found"
+            result.append(data)
         return result
     
     def meguminime(self):
         result=[]
         try:
-            cnx=requests.get(self.url["meguminime"],params=self.query["meguminime"])
+            cnx=requests.get(self.url["meguminime"],params=self.query["meguminime"], headers=self.header)
             soup=BeautifulSoup(cnx.text, "lxml")
             div=soup.find(class_="pst")
             link=div.find_all("h2")
             for h2 in link:
-                result.append(h2.a["href"])
+                data={}
+                data["link"]=h2.a["href"]
+                data["title"]=h2.a["title"]
+                result.append(data)
         except:
-            result.append("404 Not Found")
+            data={}
+            data["link"]="404 Not Found"
+            data["title"]="404 Not Found"
+            result.append(data)
         return result
     
     def drivenime(self):
         result=[]
         try:
-            cnx=requests.get(self.url["drivenime"],params=self.query["drivenime"])
+            cnx=requests.get(self.url["drivenime"],params=self.query["drivenime"], headers=self.header)
             soup=BeautifulSoup(cnx.text, "lxml")
             div=soup.find(id="content_box")
             link=div.find_all("h2")
             for h2 in link:
-                result.append(h2.a["href"])
+                data={}
+                data["link"]=h2.a["href"]
+                data["title"]=h2.a["title"]
+                result.append(data)
         except:
-            result.append("404 Not Found")
+            data={}
+            data["link"]="404 Not Found"
+            data["title"]="404 Not Found"
+            result.append(data)
         return result
     
     def bakacan(self):
         result=[]
         try:
-            cnx=requests.get(self.url["bakacan"],params=self.query["bakacan"])
+            cnx=requests.get(self.url["bakacan"],params=self.query["bakacan"], headers=self.header)
             soup=BeautifulSoup(cnx.text, "lxml")
             div=soup.find(class_="vinsmokebody")
             link=div.find_all("h2")
             for h2 in link:
-                result.append(h2.a["href"])
+                data={}
+                data["link"]=h2.a["href"]
+                data["title"]=h2.a["title"]
+                result.append(data)
         except:
-            result.append("404 Not Found")
+            data={}
+            data["link"]="404 Not Found"
+            data["title"]="404 Not Found"
+            result.append(data)
         return result
     
     def meownime(self):
         result=[]
         try:
-            cnx=requests.get(self.url["meownime"],params=self.query["meownime"])
+            cnx=requests.get(self.url["meownime"],params=self.query["meownime"], headers=self.header)
             soup=BeautifulSoup(cnx.text, "lxml")
             div=soup.find(class_="site-main")
             link=div.find_all(class_="entry-title")
             for h2 in link:
-                result.append(h2.a["href"])
+                data={}
+                data["link"]=h2.a["href"]
+                data["title"]=h2.a["title"]
+                result.append(data)
         except:
-            result.append("404 Not Found")
+            data={}
+            data["link"]="404 Not Found"
+            data["title"]="404 Not Found"
+            result.append(data)
         return result
     
     def wibudesu(self):
         result=[]
         try:
-            cnx=requests.get(self.url["wibudesu"],params=self.query["wibudesu"])
+            cnx=requests.get(self.url["wibudesu"],params=self.query["wibudesu"], headers=self.header)
             soup=BeautifulSoup(cnx.text, "lxml")
             div=soup.find(class_="rseries")
             link=div.find_all("h2")
             for h2 in link:
-                result.append(h2.a["href"])
+                data={}
+                data["link"]=h2.a["href"]
+                data["title"]=h2.a["title"]
+                result.append(data)
         except:
-            result.append("404 Not Found")
+            data={}
+            data["link"]="404 Not Found"
+            data["title"]="404 Not Found"
+            result.append(data)
         return result
+
     def kusonime(self):
         result=[]
         try:
-            cnx=requests.get(self.url["kusonime"],params=self.query["kusonime"])
+            cnx=requests.get(self.url["kusonime"],params=self.query["kusonime"], headers=self.header)
             soup=BeautifulSoup(cnx.text, "lxml")
             div=soup.find(class_="rseries")
             link=div.find_all("h2")
             for h2 in link:
-                result.append(h2.a["href"])
+                data={}
+                data["link"]=h2.a["href"]
+                data["title"]=h2.a["title"]
+                result.append(data)
         except:
-            result.append("404 Not Found") 
+            data={}
+            data["link"]="404 Not Found"
+            data["title"]="404 Not Found"
+            result.append(data) 
         return result
 
     def awbatch(self):
         result=[]
         try:
-            cnx=requests.get(self.url["awbatch"],params=self.query["awbatch"])
+            cnx=requests.get(self.url["awbatch"],params=self.query["awbatch"], headers=self.header)
             soup=BeautifulSoup(cnx.text, "lxml")
             div=soup.find(class_="konten-tengah")
             link=div.find_all("h2")
             for h2 in link:
-                result.append(h2.a["href"])
+                data={}
+                data["link"]=h2.a["href"]
+                data["title"]=h2.a["title"]
+                result.append(data)
         except:
-            result.append("404 Not Found") 
+            data={}
+            data["link"]="404 Not Found"
+            data["title"]="404 Not Found"
+            result.append(data) 
         return result
 
+# untuk cli
 def command(argv=None):
     parse=argparse.ArgumentParser(description="tools untuk mencari link download anime")
-    parse.add_argument("-s","--site",help="untuk menentukan website mana yang akan di ambil datanya. jika parameter site tidak diisi maka akan dicari diseluruh website yang ada(1.animesave 2.meguminime 3.drivenime 4.bakacan 5.meownime 6.wibudesu 7.kusonime)")
+    parse.add_argument("-s","--site",help="untuk menentukan website mana yang akan di ambil datanya. jika parameter site tidak diisi maka akan dicari diseluruh website yang ada")
     parse.add_argument("-k","--keyword",help="kata kunci untuk mencari link")
+    parse.add_argument("-ls","--listSite",help="daftar website yang dapat di ambil datanya",action="store_true")
 
     result=parse.parse_args(argv)
-    return(result.site,result.keyword)
+    return(result.site,result.keyword,result.listSite)
 
 if __name__ == "__main__":
-    site, keyword=command(sys.argv[1:])
-    anime=animeLink()
-    anime.search(keyword)
+    site, keyword,listSite=command(sys.argv[1:])
     result={}
-
-    if site == "animesave":
-        result["animesave"]=anime.animesave()
-    elif site == "meguminime":
-        result["meguminime"]=anime.meguminime()
-    elif site == "drivenime":
-        result["drivenime"]=anime.drivenime()
-    elif site == "bakacan":
-        result["bakacan"]=anime.bakacan()
-    elif site == "meownime":
-        result["meownime"]=anime.meownime()
-    elif site == "wibudesu":
-        result["wibudesu"]=anime.wibudesu()
-    elif site == "kusonime":
-        result["kusonime"]=anime.kusonime()
-    elif site == "awbatch":
-        result["awbatch"]=anime.awbatch()
+    banner="""
+             _  ____           _                  
+  __ _ _ __ (_)/ ___|_ __ __ _| |__   _ __  _   _ 
+ / _` | '_ \| | |  _| '__/ _` | '_ \ | '_ \| | | |
+| (_| | | | | | |_| | | | (_| | |_) || |_) | |_| |
+ \__,_|_| |_|_|\____|_|  \__,_|_.__(_) .__/ \__, |
+  author: Nabil                      |_|    |___/ 
+  link: https://github.com/nabil48
+"""
+    print(Fore.LIGHTCYAN_EX,banner,Fore.RESET)
+    if listSite:
+        print("[+] daftar website\n"
+        "\t[1] animesave\n"\
+        "\t[2] meguminime\n"\
+        "\t[3] drivenime\n"\
+        "\t[4] bakacan\n"\
+        "\t[5] meownime\n"\
+        "\t[6] wibudesu\n"\
+        "\t[7] kusonime\n"\
+        "\t[8] awbatch")
     else:
-        result["animesave"]=anime.animesave()
-        time.sleep(1)
-        result["meguminime"]=anime.meguminime()
-        time.sleep(1)
-        result["drivenime"]=anime.drivenime()
-        time.sleep(1)
-        result["bakacan"]=anime.bakacan()
-        time.sleep(1)
-        result["meownime"]=anime.meownime()
-        time.sleep(1)
-        result["wibudesu"]=anime.wibudesu()
-        time.sleep(1)
-        result["kusonime"]=anime.kusonime()
-        time.sleep(1)
-        result["awbatch"]=anime.awbatch()
-
+        anime=animeLink()
+        anime.search(keyword.lower())
+        if site == "animesave":
+            result["animesave"]=anime.animesave()
+        elif site == "meguminime":
+            result["meguminime"]=anime.meguminime()
+        elif site == "drivenime":
+            result["drivenime"]=anime.drivenime()
+        elif site == "bakacan":
+            result["bakacan"]=anime.bakacan()
+        elif site == "meownime":
+            result["meownime"]=anime.meownime()
+        elif site == "wibudesu":
+            result["wibudesu"]=anime.wibudesu()
+        elif site == "kusonime":
+            result["kusonime"]=anime.kusonime()
+        elif site == "awbatch":
+            result["awbatch"]=anime.awbatch()
+        else:
+            result["animesave"]=anime.animesave()
+            time.sleep(1)
+            result["meguminime"]=anime.meguminime()
+            time.sleep(1)
+            result["drivenime"]=anime.drivenime()
+            time.sleep(1)
+            result["bakacan"]=anime.bakacan()
+            time.sleep(1)
+            result["meownime"]=anime.meownime()
+            time.sleep(1)
+            result["wibudesu"]=anime.wibudesu()
+            time.sleep(1)
+            result["kusonime"]=anime.kusonime()
+            time.sleep(1)
+            result["awbatch"]=anime.awbatch()
     for website in result:
         print("[+] %s"%website)
         for index in result[website]:
-            print("\t[↪] %s"%index)
+            print(Fore.YELLOW,"\t[↪] %s"%index["title"],Fore.RESET)
+            print("\t    %s"%index["link"])
